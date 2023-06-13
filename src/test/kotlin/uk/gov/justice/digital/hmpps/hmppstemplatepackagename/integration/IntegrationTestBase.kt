8cb34e84584f4135a763bc77b3a89cda8b9c8dc9
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2023-06-13 09:53:00 +0100

    adding component scan for integration test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppstemplatepackagename/integration/IntegrationTestBase.kt
@@ -5,8 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.HmppsLaunchpadAuth
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = [HmppsLaunchpadAuth::class])
 @ActiveProfiles("test")
 abstract class IntegrationTestBase {
 
