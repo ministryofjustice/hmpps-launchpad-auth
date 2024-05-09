29ca646dddb398a1ca7694545a07617a0334eb8c
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2024-05-09 11:42:51 +0100

    dependencies version updates

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/analytics/AppInsightEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/analytics/AppInsightEventType.kt
@@ -2,5 +2,5 @@ package uk.gov.justice.digital.hmpps.hmppslaunchpadauth.analytics
 
 enum class AppInsightEventType {
   TOKEN_GENERATED_VIA_AUTHORIZATION_CODE,
-  TOKEN_GENERATED_VIA_REFRESH_TOKEN, ;
+  TOKEN_GENERATED_VIA_REFRESH_TOKEN,
 }
