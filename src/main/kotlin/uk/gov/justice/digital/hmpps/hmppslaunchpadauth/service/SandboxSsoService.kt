a8b7fe0477fd682a3912856841c5326b438c542a
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2024-07-30 10:57:01 +0100

    updating userclaims model class for booking and establishment

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/service/SandboxSsoService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/service/SandboxSsoService.kt
@@ -1,19 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service
 
-import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.Booking
-import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.Establishment
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.User
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.UserClaims
-import java.util.UUID
 
 // @Service
 class SandboxSsoService() {
   companion object {
     fun getThirdPartyTestUser(): UserClaims {
       val user = User("random_user@test.com", "John", "Smith")
-      val booking = Booking(UUID.randomUUID().toString())
-      val establishment = Establishment("EEI", "Test Establishment", "Test Establishment", false)
-      return UserClaims(booking, establishment, user)
+      // val booking = Booking(UUID.randomUUID().toString())
+      // val establishment = Establishment("EEI", "Test Establishment", "Test Establishment", false)
+      return UserClaims(null, null, user)
     }
   }
 }
