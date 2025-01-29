4f1d40f48a8e4621018ea4040a152d1b021fda08
Varun Kumar <varun.kumar@digital.justice.gov.uk>
2025-01-29 05:39:03 +0000

    adding nutrition app ip address in prod for allowlist and providing test establishment information for third party test account jwt tokens

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/service/SandboxSsoService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/service/SandboxSsoService.kt
@@ -1,13 +1,24 @@
 package uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service
 
+import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.model.Scope
+import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.Establishment
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.User
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.UserClaims
 
 class SandboxSsoService() {
   companion object {
-    fun getThirdPartyTestUser(userId: String): UserClaims {
-      val user = User(userId, "John", "Smith")
-      return UserClaims(null, null, user)
+    fun getThirdPartyTestUser(userId: String, scopes: Set<Scope>): UserClaims {
+      val user = User(userId, "John","Smith")
+      var establishment: Establishment? = null
+      if (scopes.contains(Scope.USER_ESTABLISHMENT_READ)) {
+        establishment = Establishment(
+          "HTE",
+          "Test Establishment",
+          "HMP TestEstablishment",
+          false
+          )
+      }
+      return UserClaims(null, establishment, user)
     }
   }
 }
