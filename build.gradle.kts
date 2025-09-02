438ffe2c8f7a219e340186889d0b6017516cb089
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2025-09-02 12:39:47 +0100

    Feature lnp 1196 migrate api to GitHub actions (#134)
    * Migrate to Github action
    
    * Update pipeline.yml
    
    Change to build image on branch
    
    * Update pipeline.yml
    
    Deploy build to dev env only
    
    * Update pipeline.yml
    
    Removed temporary testing on depoyment to dev
    
    * Dependency updates
    
    Dependency updates, upgrade gradle version and use datafeedUrl for dependency check
    
    * Update values-dev.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
   kotlin("plugin.spring") version "2.2.0"
   kotlin("plugin.jpa") version "2.2.0"
+  id("org.owasp.dependencycheck") version "12.1.3"
 }
 
 configurations {
@@ -48,4 +49,8 @@ tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
   }
+}
+
+dependencyCheck {
+  nvd.datafeedUrl = "file:///opt/vulnz/cache"
 }
