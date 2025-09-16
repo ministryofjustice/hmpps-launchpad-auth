5f6203293951cd8f65b6198175d4f2f5515898ce
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2025-09-16 09:34:29 +0100

    feature-migrate-health-endpoint-based-on-latest-kotling-template

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/config/WebClientConfiguration.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.hmppslaunchpadauth.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.hmpps.kotlin.auth.healthWebClient
+import java.time.Duration
+
+@Configuration
+class WebClientConfiguration(
+  @param:Value("\${launchpad.auth.iss-url}") val hmppsAuthBaseUri: String,
+  @param:Value("\${hmpps.prison-api.url}") val hmppsPrisonApiBaseUri: String,
+  @param:Value("\${api.health-timeout:2s}") val healthTimeout: Duration,
+) {
+
+  @Bean
+  fun hmppsAuthHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(hmppsAuthBaseUri, healthTimeout)
+
+  @Bean
+  fun prisonApiHealthWebClient(builder: WebClient.Builder): WebClient = builder.healthWebClient(hmppsPrisonApiBaseUri, healthTimeout)
+}
