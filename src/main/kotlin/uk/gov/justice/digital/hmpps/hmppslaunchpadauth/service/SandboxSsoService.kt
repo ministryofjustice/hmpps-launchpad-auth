d7e3ab64c24ed4dfa093e1c386fd3a0a1bb68c0d
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2025-10-13 11:52:02 +0100

    Update SandboxSsoService.kt (#144)
    * Update SandboxSsoService.kt
    
    * Update SandboxSsoService.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/service/SandboxSsoService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/service/SandboxSsoService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service
 
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.model.Scope
+import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.Booking
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.Establishment
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.User
 import uk.gov.justice.digital.hmpps.hmppslaunchpadauth.service.integration.prisonerapi.model.UserClaims
@@ -9,16 +10,28 @@ class SandboxSsoService {
   companion object {
     fun getThirdPartyTestUser(userId: String, scopes: Set<Scope>): UserClaims {
       val user = User(userId, "John", "Smith")
-      var establishment: Establishment? = null
-      if (scopes.contains(Scope.USER_ESTABLISHMENT_READ)) {
-        establishment = Establishment(
-          "HTE",
-          "Test Establishment",
-          "HMP TestEstablishment",
-          false,
-        )
+      return when {
+        scopes.contains(Scope.USER_BOOKING_READ) -> {
+          val booking = Booking("12345")
+          val establishment = Establishment(
+            "BNI",
+            "Test Establishment",
+            "HMP TestEstablishment",
+            false,
+          )
+          UserClaims(booking, establishment, user)
+        }
+        scopes.contains(Scope.USER_ESTABLISHMENT_READ) -> {
+          val establishment = Establishment(
+            "HTE",
+            "Test Establishment",
+            "HMP TestEstablishment",
+            false,
+          )
+          UserClaims(null, establishment, user)
+        }
+        else -> UserClaims(null, null, user)
       }
-      return UserClaims(null, establishment, user)
     }
   }
 }
