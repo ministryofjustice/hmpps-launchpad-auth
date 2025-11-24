b27d3f4c1500cd6905b469c0be362b964dc2f2f1
BOBY JACOB <Boby.Jacob@justice.gov.uk>
2025-11-24 10:38:02 +0000

    Removed running container check

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/utils/PostgresContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppslaunchpadauth/utils/PostgresContainer.kt
@@ -2,31 +2,17 @@ package uk.gov.justice.digital.hmpps.hmppslaunchpadauth.utils
 
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.containers.wait.strategy.Wait
-import java.io.IOException
-import java.net.ServerSocket
 
 object PostgresContainer {
   val repositoryContainer: PostgreSQLContainer<Nothing>? by lazy { startPostgresqlContainer() }
 
-  private fun startPostgresqlContainer(): PostgreSQLContainer<Nothing>? {
-    if (isPostgresRunning()) {
-      //return null
-    }
-    return PostgreSQLContainer<Nothing>("postgres:17").apply {
-      withEnv("HOSTNAME_EXTERNAL", "localhost")
-      withDatabaseName("launchpad-test")
-      withUsername("launchpad")
-      withPassword("launchpad")
-      setWaitStrategy(Wait.forListeningPort())
-      withReuse(true)
-      start()
-    }
-  }
-
-  private fun isPostgresRunning(): Boolean = try {
-    val serverSocket = ServerSocket(5432)
-    serverSocket.localPort == 0
-  } catch (error: IOException) {
-    true
+  private fun startPostgresqlContainer(): PostgreSQLContainer<Nothing>? = PostgreSQLContainer<Nothing>("postgres:17").apply {
+    withEnv("HOSTNAME_EXTERNAL", "localhost")
+    withDatabaseName("launchpad-test")
+    withUsername("launchpad")
+    withPassword("launchpad")
+    setWaitStrategy(Wait.forListeningPort())
+    withReuse(true)
+    start()
   }
 }
