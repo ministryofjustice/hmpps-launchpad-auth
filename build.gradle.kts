396309d67e47b2630043c4600a7e4ae250959684
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-07-01 13:23:45 +0100

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.4.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.3"
   kotlin("plugin.spring") version "2.4.0"
   kotlin("plugin.jpa") version "2.4.0"
   id("org.owasp.dependencycheck") version "12.2.2"
@@ -19,8 +19,8 @@ dependencies {
   implementation("io.jsonwebtoken:jjwt-api:0.13.0")
   implementation("io.jsonwebtoken:jjwt-impl:0.13.0")
   implementation("io.jsonwebtoken:jjwt-jackson:0.13.0")
-  implementation("org.json:json:20250517")
-  implementation("org.postgresql:postgresql:42.7.11")
+  implementation("org.json:json:20260522")
+  implementation("org.postgresql:postgresql:42.7.12")
   implementation("org.ehcache:ehcache:3.12.0")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3") {
     exclude(group = "org.yaml", module = "snakeyaml")
@@ -32,7 +32,7 @@ dependencies {
   implementation("com.fasterxml.uuid:java-uuid-generator:5.2.0")
   implementation("com.nimbusds:nimbus-jose-jwt:10.9.1")
 
-  runtimeOnly("org.flywaydb:flyway-database-postgresql:12.8.1")
+  runtimeOnly("org.flywaydb:flyway-database-postgresql:12.10.0")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
